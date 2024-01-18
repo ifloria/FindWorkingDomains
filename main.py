e2c4fb96a372826990555a6aa83eecf2ab96df40
ifloria <139184487+ifloria@users.noreply.github.com>
2024-01-19 01:46:28 +0300

    main.py

--- main.py
+++ main.py
@@ -13,10 +13,12 @@ def make_request(url):
 
 with open(input("Enter a list: "),"r",encoding="utf-8") as domain_list:
     save_file = "found.txt"
+
     for word in domain_list:
         word = word.strip()
         url = "http://" + word
         response = make_request(url)
+        
         if response and response.status_code == 200:
             print(Fore.GREEN + "yea ->" + url)
         else:
